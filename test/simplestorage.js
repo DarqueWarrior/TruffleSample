b6a3dfaf83db56607b6167aea83900d9311adf87
Donovan Brown <dlb@dlbonline.com>
2022-05-15 17:30:35 +0000

    Added artifacts to GitHub workflow

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,15 +1,18 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 contract("SimpleStorage", accounts => {
-  it("It should store the value 89", async () => {
+  it("It should store the value", async () => {
+    // Arrange
+    const expected = 89;
     const simpleStorageInstance = await SimpleStorage.deployed();
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    // Act
+    // Set value
+    await simpleStorageInstance.set(expected, { from: accounts[0] });
 
+    // Assert
     // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
-
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    const actual = await simpleStorageInstance.get.call();
+    assert.equal(actual, expected, `The value ${expected} was not stored.`);
   });
 });
