c510d24b5e0570f187538daebd670b6fe00fb25d
Donovan Brown <dlb@dlbonline.com>
2022-05-16 04:53:27 +0000

    forgot powerhell file

---
+++ iac/deploy.ps1
@@ -0,0 +1,39 @@
+[CmdletBinding()]
+param (
+        [Parameter(
+                Position = 0,
+                HelpMessage = "The name of the resource group to be created. All resources will be place in the resource group and start with name."
+        )]
+        [string]
+        $rgName = "swa_demo",
+
+        [Parameter(
+                Position = 1,
+                HelpMessage = "The location to store the meta data for the deployment."
+        )]
+        [string]
+        $location = "centralus",
+        
+        [string]
+        $repoUrl
+)
+
+Write-Verbose $repoUrl
+
+Write-Output 'Deploying the Azure infrastructure'
+$deployment = $(az deployment sub create --name $rgName `
+                --location $location `
+                --template-file ./main.bicep `
+                --parameters location=$location `
+                --parameters rgName=$rgName `
+                --parameters repoUrl=$repoUrl `
+                --output json) | ConvertFrom-Json
+
+# Store the outputs from the deployment
+$deploymentToken = $deployment.properties.outputs.deploymentToken.value
+
+# Write the values as output so they can be used in other stages.
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/process/expressions?view=azure-devops#dependencies
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=azure-devops&tabs=yaml%2Cbatch#share-variables-across-pipelines
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/process/expressions?view=azure-devops#dependencies
+Write-Host "##vso[task.setvariable variable=deploymentToken;isOutput=true]$deploymentToken"
